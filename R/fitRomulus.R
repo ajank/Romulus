11b37326452f9bac9ff7a0ff71612019d12ca77f
Aleksander Jankowski <aleksander.jankowski@embl.de>
2016-06-09 14:34:11 +0200

    Corrected the manual

--- R/fitRomulus.R
+++ R/fitRomulus.R
@@ -6,7 +6,7 @@
 #' @param priors character vector or list of character vectors specifying the names of columns from \code{anno} to be considered in the logistic regression. If a list, each item specifies the column names for each bound state separately, otherwise the same column names will be used for all the bound states.
 #' @param bins1 integer vector or matrix specifying how the columns of \code{cuts1} are grouped into bins. If a matrix, each row specifies the grouping for each bound state separately, otherwise the same grouping will be used for all the bound states. The numbers specifying the grouping must form the set of the first N natural numbers (\eqn{1}, \eqn{\dots}, \eqn{N}) for some \eqn{N}.
 #' @param bins2 integer vector or matrix specifying how the columns of \code{cuts2} are grouped into bins, as above.
-#' @param nbound optional integer, specifying the number of bound states. If not provided, the value will be guessed from the length of \code{anno} or the number of rows in \code{bins1} and \code{bins2}.
+#' @param nbound optional integer, specifying the number of bound states. If not provided, the value will be guessed from the length of \code{priors} or the number of rows in \code{bins1} and \code{bins2}.
 #' @param PriorLik optional numeric matrix, with a row for each candidate binding site and a column for each bound state, containing the initial prior likelihoods. If not provided, the default initialization procedure will be used.
 #' @param addIntercept logical. Should an additional intercept term be included in the model?
 #' @param maxIter integer. Maximal number of Expectation-Maximization iterations to perform.
